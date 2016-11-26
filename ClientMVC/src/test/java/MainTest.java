d627d325b148f448d619c8deca058f26c266d436
StarOfDeath <hectorfernandoruiz@gmail.com>
2016-11-26 16:22:50 -0600

    masintentos

--- ClientMVC/src/test/java/MainTest.java
+++ ClientMVC/src/test/java/MainTest.java
@@ -66,7 +66,7 @@ public void newConnection(){
                     if (strMsg.equals("close"))
                         break;
                     assertEquals("hola", strMsg);
-                   
+
             }
         } catch (Exception e) {
 
